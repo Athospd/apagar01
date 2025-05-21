575c6869fac3da0c610957f2710186e5ebfb8447
Athos Petri Damiani <athos.damiani@gmail.com>
2025-05-21 18:16:37 -0300

    readme pipeline setup

--- rascunho.R
+++ rascunho.R
@@ -1,2 +1,5 @@
-usethis::use_github_action("render-rmarkdown")
+usethis::use_git()
+usethis::use_github()
 usethis::use_readme_rmd()
+# personaliza o README.Rmd na mao
+usethis::use_github_action("render-rmarkdown")
